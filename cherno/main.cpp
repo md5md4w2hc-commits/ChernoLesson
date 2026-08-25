b0155cfe99439142c319ba6b2b30452bfb0017f6
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-08-25 10:26:10 +0530

    Object Life Time

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -1,66 +1,43 @@
 #include <iostream>
 #include <string>
 
-struct Vector2
+class Entity
 {
-    float x, y;
-    
-    Vector2(float x, float y)
-    : x(x), y(y) {}
-    
-    Vector2 Add(const Vector2& other) const
+public:
+    Entity()
     {
-        return Vector2(x + other.x, y + other.y);
+        std::cout << "Created Entity !" << std::endl;
     }
     
-    Vector2 operator+(const Vector2& other) const
+    ~Entity()
     {
-        return Add(other);
+        std::cout << "Destroyed Entity !" << std::endl;
     }
-    
-    Vector2 Multiply(const Vector2& other) const
-    {
-        return Vector2(x * other.x, y * other.y);
-    }
-    
-    Vector2 operator*(const Vector2& other) const
-    {
-        return Multiply(other);
-    }
-    
-    bool operator==(const Vector2& other) const
-    {
-        return x == other.x && y == other.y;
-    }
-    
-    bool operator!=(const Vector2& other) const
-    {
-        return !(*this == other);
-    }
-    
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other)
+class ScopedPtr
 {
-    stream << other.x << ", " << other.y;
-    return stream;
-}
-
-int main()
-{
-    Vector2 postion(4.0f , 4.0f);
-    Vector2 speed(0.5f , 1.5f);
-    Vector2 powerup(1.1f , 1.1f);
-    
-    Vector2 result1 = postion.Add(speed.Multiply(powerup));
-    Vector2 result2 = postion + speed * powerup;
-    
-    if (result1 == result2)
+private:
+    Entity* m_Ptr;
+public:
+    ScopedPtr(Entity* ptr)
+        : m_Ptr(ptr)
     {
         
     }
     
-    std::cout << result2 << std::endl;
+    ~ScopedPtr()
+    {
+        delete m_Ptr;
+    }
+};
+
+int main()
+{
+    {
+        ScopedPtr e = new Entity();
+    }
+    
     
     std::cin.get();
 }
