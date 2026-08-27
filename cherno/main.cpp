2b8be728dce24160a01d10f162f47fd3c329f96d
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-08-27 16:18:10 +0530

    vectors

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-struct Vector3
+struct Vertex
 {
     float x, y, z;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
+{
+    stream << vertex.x << "," << vertex.y << "," << vertex.z;
+    return stream;
+}
+
+void Function(const std::vector<Vertex>& vertices)
+{
+    
+}
+
 int main()
 {
-    int offset = (int)&((Vector3*)nullptr)->x;
-    std::cout << offset << std::endl;
+    std::vector<Vertex> vertices;
+    vertices.push_back({1, 2, 3});
+    vertices.push_back({4, 5, 6});
+    Function(vertices);
+    
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        std::cout << vertices[i] << std::endl;
+    }
+    
+    vertices.erase(vertices.begin() + 1);
+    
+    for (Vertex& v : vertices)
+        {
+            std::cout << v << std::endl;
+        }
     
     std::cin.get();
 }
