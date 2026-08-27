d2d8f1b57c5391c7941804b831ccad2379f70f46
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-08-27 16:38:07 +0530

    optimization of vectors

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -5,37 +5,27 @@
 struct Vertex
 {
     float x, y, z;
-};
-
-std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
-{
-    stream << vertex.x << "," << vertex.y << "," << vertex.z;
-    return stream;
-}
-
-void Function(const std::vector<Vertex>& vertices)
-{
     
-}
+    Vertex(float x, float y, float z)
+        : x(x), y(y), z(z)
+    {
+    }
+    
+    Vertex(const Vertex& vertex)
+        : x(vertex.x), y(vertex.y), z(vertex.z)
+    {
+        std::cout << "copied!" << std::endl;
+    }
+};
 
 int main()
 {
     std::vector<Vertex> vertices;
-    vertices.push_back({1, 2, 3});
-    vertices.push_back({4, 5, 6});
-    Function(vertices);
-    
-    for (int i = 0; i < vertices.size(); i++)
-    {
-        std::cout << vertices[i] << std::endl;
-    }
-    
-    vertices.erase(vertices.begin() + 1);
-    
-    for (Vertex& v : vertices)
-        {
-            std::cout << v << std::endl;
-        }
+    vertices.reserve(3);
+    vertices.emplace_back(1, 2, 3);
+    vertices.emplace_back(4, 5, 6);
+    vertices.emplace_back(7, 8, 9);
+
     
     std::cin.get();
 }
